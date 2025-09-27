597b58854828edaca3fe42c3400dd9999b21be77
Shubhankar <shubhankarbadhale963@gmail.com>
2025-09-27 11:53:55 +0530

    forth commit

--- Abstract.java
+++ Abstract.java
@@ -1,5 +1,39 @@
-public class Abstract {
+// Abstract parent class
+abstract class Bank {
+    abstract public void intrestRate();
+}
+
+// Concrete child classes
+class HDFC extends Bank {
+    @Override
+    public void intrestRate() {
+        System.out.println("HDFC Interest Rate: 7.5%");
+    }
+}
+
+class SBI extends Bank {
+    @Override
+    public void intrestRate() {
+        System.out.println("SBI Interest Rate: 6.8%");
+    }
+}
+
+class ICICI extends Bank {
+    @Override
+    public void intrestRate() {
+        System.out.println("ICICI Interest Rate: 7.2%");
+    }
+}
+
+// Main abstract class
+public abstract class Abstract {
     public static void main(String[] args) {
-        
+        Bank hdfc = new HDFC();
+        Bank sbi = new SBI();
+        Bank icici = new ICICI();
+
+        hdfc.intrestRate();
+        sbi.intrestRate();
+        icici.intrestRate();
     }
 }
